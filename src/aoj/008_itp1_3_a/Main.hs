b97ce7855731c448fe4a90f58eb6d4537a1a6641
disk-inue <disk.inue.01@gmail.com>
2025-12-26 12:17:02 +0900

    feat : Day8

---
+++ src/aoj/008_itp1_3_a/Main.hs
@@ -0,0 +1,16 @@
+-- printHello :: Int -> Int -> IO ()
+-- printHello nextCount maxCount
+--   | nextCount > maxCount = do
+--       putStrLn "Hello World"
+--   | otherwise = do
+--       putStrLn "Hello World"
+--       printHello (nextCount + 1) maxCount
+
+printHello :: Int -> IO ()
+printHello 0 = pure ()
+printHello n = do
+  putStrLn "Hello World"
+  printHello (n - 1)
+
+main :: IO ()
+main = printHello 1000
